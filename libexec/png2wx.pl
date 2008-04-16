2b7436d037f79d5e2092e668e8e77ccdecb0f9e2
Jan Engelhardt <jengelh@computergmbh.de>
2008-04-16 21:33:37 +0200

    png2wx: fix empty #include in output file

--- libexec/png2wx.pl
+++ libexec/png2wx.pl
@@ -11,14 +11,13 @@ use strict;
 
 &main(\@ARGV);
 
-#------------------------------------------------------------------------------
-sub main($)
+sub main
 {
 	my($cpp_file, $hpp_file, $hpp_include, $Marker, $main, $tmp);
 	&Getopt::Long::Configure(qw(bundling));
 	&GetOptions(
-		"C=s"  => \$cpp_file,
-		"H=s"  => \$hpp_file,
+		"C=s" => \$cpp_file,
+		"H=s" => \$hpp_file,
 		"M=s" => \$Marker,
 	);
 
@@ -26,7 +25,8 @@ sub main($)
 		die "You need to specify -C, -H and -M options.\n";
 	}
 
-	$hpp_include = ($hpp_file =~ m{/([^/]*)$})[0];
+	$hpp_include = $hpp_file;
+	$hpp_include =~ s{^(.*)/}{};
 
 	#
 	# C++ header
@@ -99,7 +99,8 @@ extern void initialize_images(void);
 	return;
 }
 
-sub encoded {
+sub encoded
+{
 	my $file = shift @_;
 	my $data;
 	local *FH;
@@ -113,7 +114,7 @@ sub encoded {
 	$data = join(undef, <FH>);
 	$data =~ s/\\/\\\\/go;
 	$data =~ s/([^\x21\x23-\x7e])/sprintf "\\%03o", ord $1/egs;
-	$data =~ s/\?\?(?=[-\(\)<>=\/'!])/?\\077/g;
+	$data =~ s/\?\?(?=[-\(\)<>=\/\'!])/?\\077/g;
 	close FH;
 	return $data;
 }
