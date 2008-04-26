8b5ff1e77b7a7ae5a664ea5478281c19b2b83bfe
Jan Engelhardt <jengelh@computergmbh.de>
2008-04-26 20:42:15 +0200

    socketpty: must not busy-wait and automatic reconnect
    Fixed: poll(,,0) caused a busy wait.
    Also try reconnecting to the socket when connection terminates.

--- libexec/socketpty.c
+++ libexec/socketpty.c
@@ -6,6 +6,7 @@
  *	See http://jengelh.medozas.de/2008/0425-vmware-serial.php
  */
 #define _XOPEN_SOURCE 1
+#define _GNU_SOURCE 1
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -20,13 +21,8 @@
 #include <unistd.h>
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(*(x)))
 
-int main(int argc, const char **argv)
+static void do_connect(int ptmxfd, const char *socket_path)
 {
-	if (argc < 2) {
-		fprintf(stderr, "Usage: %s path-to-socket\n", *argv);
-		return EXIT_FAILURE;
-	}
-
 	int sockfd = socket(PF_UNIX, SOCK_STREAM, 0);
 	if (sockfd < 0) {
 		perror("socket");
@@ -35,36 +31,25 @@ int main(int argc, const char **argv)
 
 	struct sockaddr_un sa = {};
 	sa.sun_family = PF_UNIX;
-	strncpy(sa.sun_path, argv[1], sizeof(sa.sun_path));
+	strncpy(sa.sun_path, socket_path, sizeof(sa.sun_path));
 
 	if (connect(sockfd, (const void *)&sa, sizeof(sa)) < 0) {
 		perror("connect");
-		abort();
+		return;
 	}
-
-	int ptmxfd = open("/dev/ptmx", O_RDWR);
-	if (ptmxfd < 0) {
-		perror("open /dev/ptmx");
-		abort();
-	}
-
-	grantpt(ptmxfd);
-	unlockpt(ptmxfd);
-	printf("Slave is %s\n", ptsname(ptmxfd));
 
 	struct pollfd pollreq[2] = {
-		{.fd = ptmxfd, .events = POLLIN},
-		{.fd = sockfd, .events = POLLIN},
+		{.fd = ptmxfd, .events = POLLIN | POLLRDHUP},
+		{.fd = sockfd, .events = POLLIN | POLLRDHUP},
 	};
 
 	char buf[4096];
 
 	while (true) {
-		if (poll(pollreq, ARRAY_SIZE(pollreq), 0) < 0) {
-			if (errno == EINTR)
-				break;
+		if (poll(pollreq, ARRAY_SIZE(pollreq), -1) < 0)
 			continue;
-		}
+		if (pollreq[1].revents & POLLRDHUP)
+			break;
 		if (pollreq[0].revents & POLLIN) {
 			ssize_t rd = read(ptmxfd, buf, sizeof(buf));
 			if (rd < 0 || write(sockfd, buf, rd) < 0)
@@ -77,7 +62,31 @@ int main(int argc, const char **argv)
 		}
 	}
 
-	close(ptmxfd);
 	close(sockfd);
+}
+
+int main(int argc, const char **argv)
+{
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s path-to-socket\n", *argv);
+		return EXIT_FAILURE;
+	}
+
+	int ptmxfd = open("/dev/ptmx", O_RDWR);
+	if (ptmxfd < 0) {
+		perror("open /dev/ptmx");
+		abort();
+	}
+
+	grantpt(ptmxfd);
+	unlockpt(ptmxfd);
+	printf("Slave is %s\n", ptsname(ptmxfd));
+
+	while (true) {
+		do_connect(ptmxfd, argv[1]);
+		sleep(1);
+	}
+
+	close(ptmxfd);
 	return EXIT_SUCCESS;
 }
