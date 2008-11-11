fbfd7716ffdedcac0b10559aeaaff1258240d864
Jan Engelhardt <jengelh@medozas.de>
2008-11-11 13:33:17 +0100

    src: remove pegrep again
    pcregrep(1) seems to be the tool that is also available in
    distributions too stupid to support grep -P.

--- data/hxtools_profile.bash
+++ data/hxtools_profile.bash
@@ -30,8 +30,11 @@ if [ "$HXPREF_COLORS" == yes ]; then
 	# See if grep knows about --color, and set inverse-cyan
 	#
 	grep --color=auto x /dev/null &>/dev/null;
-	[ "$?" -ne 2 ] && alias grep="`which grep` --color=auto";
+	[ "$?" -ne 2 ] && alias grep="command grep --color=auto";
+	pcregrep --color=auto x /dev/null &>/dev/null;
+	[ "$?" -ne 2 ] && alias pcregrep="command pcregrep --color=auto";
 	export GREP_COLOR="36;7";
+	export PCREGREP_COLOR="36;7";
 fi;
 
 # Paths. Honors local setup.
