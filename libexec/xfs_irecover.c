4b4ce6590a5774041a46f658ebafc3c7084c9847
Jan Engelhardt <jengelh@medozas.de>
2009-02-04 10:32:10 +0100

    xfs_irecover: make it available under GPL2+

--- libexec/xfs_irecover.c
+++ libexec/xfs_irecover.c
@@ -1,12 +1,12 @@
 /*
  *	Bulk XFS Undelete
- *	Copyright C Jan Engelhardt <jengelh [at] medozas de>
+ *	Copyright C Jan Engelhardt <jengelh [at] medozas de>, 2008 - 2009
  *
  *	This program is free software; you can redistribute it and/or
  *	modify it under the terms of the GNU General Public License
- *	as published by the Free Software Foundation; either version 3
+ *	as published by the Free Software Foundation; either version 2
  *	of the License, or (at your option) any later version.
- *	For details, see the file named "LICENSE.GPL3".
+ *	For details, see the file named "LICENSE.GPL2".
  *
  *	The program extracts all inodes (with some bounding possible by
  *	means of specifying command line options).
