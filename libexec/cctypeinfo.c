e7078353b82f8682f9af6ae5659129fdacee5c04
Jan Engelhardt <jengelh@medozas.de>
2009-06-16 07:57:03 +0200

    cctypeinfo: add more types
    When cross-compiling, compilation of main may not be possible (due to
    missing libc, for example), so also provide the results in variables
    that can be read with objdump.

--- libexec/cctypeinfo.c
+++ libexec/cctypeinfo.c
@@ -6,9 +6,51 @@
 #include <stdlib.h>
 #include <wchar.h>
 
+struct x16 {
+	uint8_t a;
+	uint16_t b;
+};
+
+struct x32 {
+	uint8_t a;
+	uint16_t b;
+	uint32_t c;
+};
+
+struct x64 {
+	uint8_t a;
+	uint16_t b;
+	uint32_t c;
+	uint64_t d;
+};
+
+#define p(type) q(type, type)
+#define q(type, vname) \
+	extern int SIZEOF_##vname, ALIGNOF_##vname; \
+	int SIZEOF_##vname = sizeof(type), ALIGNOF_##vname = __alignof__(type);
 #define t(type) \
 	printf("%14s  %7zu  %7zu\n", #type, sizeof(type), __alignof__(type))
 
+p(char);
+p(short);
+p(int);
+p(long);
+q(long long, longlong);
+p(float);
+p(double);
+q(long double, longdouble);
+q(void *, voidptr);
+q(void (*)(void), funcptr);
+p(wchar_t);
+p(uint8_t);
+p(uint16_t);
+p(uint32_t);
+p(uint64_t);
+q(struct x16, x16);
+q(struct x32, x32);
+q(struct x64, x64);
+
+#ifndef WITHOUT_MAIN
 int main(void)
 {
 	printf("%14s  %7s  %7s\n", "TYPE", "SIZEOF", "ALIGNOF");
@@ -27,5 +69,9 @@ int main(void)
 	t(uint16_t);
 	t(uint32_t);
 	t(uint64_t);
+	t(struct x16);
+	t(struct x32);
+	t(struct x64);
 	return EXIT_SUCCESS;
 }
+#endif
