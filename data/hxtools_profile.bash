a34feffe692ccbde96bd00dd3262396c8f797fe3
Jan Engelhardt <jengelh@medozas.de>
2011-02-24 03:31:08 +0100

    hxlogin: use bash syntax

--- data/hxtools_profile.bash
+++ data/hxtools_profile.bash
@@ -7,19 +7,19 @@ if [ "$HXPREF_ENABLE" == "yes" ]; then
 # --- main big block ---
 
 isroot=0;
-[ "$UID" -eq 0 ] && isroot=1;
+[[ "$UID" -eq 0 ]] && isroot=1;
 
 export CVS_RSH="ssh";
-[ -n "$EDITOR" ] && export EDITOR;
+[[ -n "$EDITOR" ]] && export EDITOR;
 
-if [ "$HXPREF_COLORS" == yes ]; then
+if [[ "$HXPREF_COLORS" == yes ]]; then
 	#
 	# See if grep knows about --color, and set inverse-cyan
 	#
 	grep --color=auto x /dev/null &>/dev/null;
-	[ "$?" -ne 2 ] && alias grep="command grep --color=auto";
+	[[ "$?" -ne 2 ]] && alias grep="command grep --color=auto";
 	pcregrep --color=auto x /dev/null &>/dev/null;
-	[ "$?" -ne 2 ] && alias pcregrep="command pcregrep --color=auto";
+	[[ "$?" -ne 2 ]] && alias pcregrep="command pcregrep --color=auto";
 	export GREP_COLOR="36;7";
 	export PCREGREP_COLOR="36;7";
 fi;
