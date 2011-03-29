ff3f4a27dc07e210aff7a59e87946e5d49d0a55e
Jan Engelhardt <jengelh@medozas.de>
2011-03-30 00:14:40 +0200

    hxprofile: hide temporary variables

--- data/hxtools_profile.bash
+++ data/hxtools_profile.bash
@@ -4,14 +4,6 @@
 
 if [ "$HXPREF_ENABLE" == "yes" ]; then
 
-# --- main big block ---
-
-isroot=0;
-[[ "$UID" -eq 0 ]] && isroot=1;
-
-export CVS_RSH="ssh";
-[[ -n "$EDITOR" ]] && export EDITOR;
-
 hxtools_profile_setsbin ()
 {
 	local IFS=":";
@@ -38,33 +30,43 @@ hxtools_profile_setsbin ()
 	PATH="${PATH:1}";
 }
 
-if [[ "$HXPREF_SBIN" == yes ]]; then
-	hxtools_profile_setsbin;
-fi;
+hxtools_profile_main ()
+{
+	local isroot=0;
+	[[ "$UID" -eq 0 ]] && isroot=1;
+
+	export CVS_RSH="ssh";
+	[[ -n "$EDITOR" ]] && export EDITOR;
+
+	if [[ "$HXPREF_SBIN" == yes ]]; then
+		hxtools_profile_setsbin;
+	fi;
+
+	if [[ "$HXPREF_COLORS" == yes ]]; then
+		#
+		# See if grep knows about --color, and set inverse-cyan
+		#
+		grep --color=auto x /dev/null &>/dev/null;
+		[[ "$?" -ne 2 ]] && alias grep="command grep --color=auto";
+		pcregrep --color=auto x /dev/null &>/dev/null;
+		[[ "$?" -ne 2 ]] && alias pcregrep="command pcregrep --color=auto";
+		export GREP_COLOR="36;7";
+		export PCREGREP_COLOR="36;7";
+	fi;
+
+	#
+	# Retain language across sudo and `bash --login` (which would otherwise
+	# re-source /etc/sysconfig/language)
+	#
+	if [[ -n "$HX_LC_MESSAGES" ]]; then
+		export LC_MESSAGES="$HX_LC_MESSAGES";
+	else
+		export HX_LC_MESSAGES="$LC_MESSAGES";
+	fi;
+}
+
+hxtools_profile_main;
+unset hxtools_profile_main;
 unset hxtools_profile_setsbin;
-
-if [[ "$HXPREF_COLORS" == yes ]]; then
-	#
-	# See if grep knows about --color, and set inverse-cyan
-	#
-	grep --color=auto x /dev/null &>/dev/null;
-	[[ "$?" -ne 2 ]] && alias grep="command grep --color=auto";
-	pcregrep --color=auto x /dev/null &>/dev/null;
-	[[ "$?" -ne 2 ]] && alias pcregrep="command pcregrep --color=auto";
-	export GREP_COLOR="36;7";
-	export PCREGREP_COLOR="36;7";
-fi;
-
-#
-# Retain language across sudo and `bash --login` (which would otherwise
-# re-source /etc/sysconfig/language)
-#
-if [[ -n "$HX_LC_MESSAGES" ]]; then
-	export LC_MESSAGES="$HX_LC_MESSAGES";
-else
-	export HX_LC_MESSAGES="$LC_MESSAGES";
-fi;
-
-# --- end big main block ---
 
 fi; # if [ "$HXPREF_ENABLE" == "yes" ];
